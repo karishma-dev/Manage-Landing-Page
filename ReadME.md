6197617310867f1f658b17ee0d79f36ab7be5116
Krrish105 <jainkarishma76@gmail.com>
2022-03-07 16:04:17 +0530

    first commit

---
+++ ReadME.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Manage landing page solution
+
+This is a solution to the [Manage landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/manage-landing-page-SLXqC6P5). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See all testimonials in a horizontal slider
+- Receive an error message when the newsletter sign up `form` is submitted if:
+  - The `input` field is empty
+  - The email address is not formatted correctly
+
+### Screenshot
+
+![](./images/screenshot.png)
+
+### Links
+
+- Solution URL: [https://github.com/Krrish105/Manage-Landing-Page](https://github.com/Krrish105/Manage-Landing-Page)
+- Live Site URL: [http://Krrish105.github.io/Manage-Landing-Page](http://Krrish105.github.io/Manage-Landing-Page)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [Swiper API](https://swiperjs.com/swiper-api)
+
+## Author
+
+- Frontend Mentor - [Krrish105](https://www.frontendmentor.io/profile/Krrish105)
+- Twitter - [_karishma10](https://twitter.com/_karishma10)
+- LinkedIn - [Karishma Garg](https://www.linkedin.com/in/karishma-garg-)
+
---
+++ script.js
@@ -0,0 +1,71 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navItem = document.querySelectorAll(".nav-item");
+const form = document.forms.email;
+const inputForm = document.getElementById("email-input");
+const error = document.querySelector(".error");
+
+hamburger.addEventListener("click", function(){
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+navItem.forEach((item) => {
+     item.addEventListener("click", function(){
+       hamburger.classList.remove("active")
+       navMenu.classList.remove("active");
+     })
+});
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    centeredSlides: true,
+    breakpoints: {
+      600: {
+        slidesPerView: 1.45,
+        spaceBetween: 0,
+      },
+      992: {
+        initialSlide: 1,
+        slidesPerView: 2.38,
+        spaceBetween: 40,
+      },
+    },
+    cssmode: true,
+    mousewheel: true,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      centeredSlides: true,
+      clickable: true,
+    },
+});
+
+let regex = /^([a-z 0-9\.-]+)@([a-z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
+// EventListener for form to show error on wrong email format
+form.addEventListener("submit", function(event){
+
+  event.preventDefault();
+
+  if(!inputForm.value){
+    error.classList.add("show");
+    inputForm.classList.add("show");
+    error.innerHTML = "Enter Email";
+    return;
+  }
+  if(!regex.test(inputForm.value)){
+    error.classList.add("show");
+    inputForm.classList.add("show");
+    error.innerHTML="Whoops, make sure it's an email";
+    return;
+  }
+
+  error.classList.remove("show");
+  inputForm.classList.remove("show");
+  error.innerHTML = "";
+  inputForm.value = "";
+  return;
+
+})
